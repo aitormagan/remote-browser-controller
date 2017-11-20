286816ec2bbfe847ba4d8c3adb2612860c9b1bb3
Aitor Magán García <amagan@conwet.com>
2017-11-21 00:42:50 +0100

    Fix tabs

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ var winston = require('winston');
 const PORT = 3000;
 
 var logger = new (winston.Logger)({
-    transports: [
-      new (winston.transports.Console)({'timestamp': true})
-    ]
+  transports: [
+    new (winston.transports.Console)({'timestamp': true})
+  ]
 });
 
 // EXPRESS CONFIGRATION
@@ -19,19 +19,19 @@ app.use(bodyParser.json());
 app.use('/web', express.static('static'));
 
 app.get('/', function(req, res) {
-	res.redirect('/web');
+  res.redirect('/web');
 });
 
 app.post("/api/show", function(req, res) {
-	logger.log('info', 'Web ' + req.body.web + ' required to be shown');
-	io.sockets.emit("new_web", req.body);
-	res.send({ok: 1});
+  logger.log('info', 'Web ' + req.body.web + ' required to be shown');
+  io.sockets.emit("new_web", req.body);
+  res.send({ok: 1});
 });
 
 io.on('connection', function(socket) {
-	logger.log('info', 'New Client is now connected!!');
+  logger.log('info', 'New Client is now connected!!');
 });
 
 server.listen(PORT, function () {
- 	logger.log('info', 'Example app listening on port ' + PORT + '!');
+   logger.log('info', 'Example app listening on port ' + PORT + '!');
 });
